1d968a8d883fcbc64fee714d7c9dd7cb5465a428
Sean Spratt <seansp@gmail.com>
2017-10-03 23:47:34 -0700

    enumerate hippie/jpl/my. simple.

---
+++ diagnostics/Collect-Azure-Diagnostics.ps1
@@ -0,0 +1,50 @@
+﻿. .\Utilities.ps1
+
+
+# Collect up the storage accounts.
+$accounts = Get-AzureRmStorageAccount
+
+$numberAccounts = $accounts.Length 
+Message "Total of $numberAccounts Accounts."
+foreach( $storageAccount in $accounts | Sort-Object Location, CreationTime, StorageAccountName )
+{
+  $include = $false;
+
+  switch -Wildcard ( $storageAccount.ResourceGroupName.ToLower() )
+  {
+    "*jpl*" { $include = $true }
+    "*hippie*" { $include = $true }
+    "*my*" { $include = $true }
+  }
+  switch -Wildcard ( $storageAccount.StorageAccountName.ToLower() )
+  {
+    "*jpl*" { $include = $true }
+    "*hippie*" { $include = $true }
+    "*my*" { $include = $true }
+  }
+
+  if( $include -eq $true )
+    {
+
+      $name = $storageAccount.StorageAccountName
+      $resourceGroup = $storageAccount.ResourceGroupName
+      $location = $storageAccount.Location
+      $creationDate = $storageAccount.CreationTime
+
+      Message "$creationDate | $location | $resourceGroup | $name"
+      
+      $key1 = (Get-AzureRmStorageAccountKey -ResourceGroupName $storageAccount.ResourceGroupName -name $storageAccount.StorageAccountName)[0].value
+      $storageContext = New-AzureStorageContext -StorageAccountName $storageAccount.StorageAccountName -StorageAccountKey $key1
+      $storageContainer = Get-AzureStorageContainer -Context $storageContext
+
+  }
+}
+
+
+
+
+
+#Import-Module ..\ConvertFrom-ArbritraryXml.psm1
+#$xmlAsText = Get-Content -Path .\deploy_Drone.xml
+#$ob = ConvertFrom-ArbritraryXml( [xml] $xmlAsText )
+#$json = $ob | ConvertTo-Json -Depth 10
---
+++ diagnostics/Utilities.ps1
@@ -0,0 +1,29 @@
+﻿
+function Message( $msg )
+{
+  $now = [Datetime]::Now.ToUniversalTime().ToString("MM/dd/yyyy HH:mm:ss")
+  Write-Host -BackgroundColor White -ForegroundColor Blue "$now| $msg"
+}
+function Error( $msg )
+{
+  $now = [Datetime]::Now.ToUniversalTime().ToString("MM/dd/yyyy HH:mm:ss : ")
+  Write-Host -BackgroundColor Red -ForegroundColor Yellow "$now| ERR | $msg"
+}
+
+Message "Loading the ProfileContext"
+try
+{
+  Import-AzureRmContext -Path 'C:\Azure\ProfileContext.ctx' -ErrorAction Stop
+  Select-AzureRmSubscription -SubscriptionId 2cd20493-fe97-42ef-9ace-ab95b63d82c4
+} catch
+{
+  Error "An error occurred while loading the context."
+  return
+} 
+
+
+
+#Import-Module ..\ConvertFrom-ArbritraryXml.psm1
+#$xmlAsText = Get-Content -Path .\deploy_Drone.xml
+#$ob = ConvertFrom-ArbritraryXml( [xml] $xmlAsText )
+#$json = $ob | ConvertTo-Json -Depth 10
